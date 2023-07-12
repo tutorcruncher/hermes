68c61b01c5f4e953b80db8006405b38869599d35
Tom Hamilton Stubber <tomhamiltonstubber@gmail.com>
2023-07-12 16:27:43 +0100

    Add bits to do next

--- app/pipedrive/views.py
+++ app/pipedrive/views.py
@@ -1,5 +1,18 @@
 async def callback():
     """
     Used to updated TC whenever a deal is updated in pipedrive.
+    If a Person changes:
+    - Do nothing in TC
+    - Do nothing in Hermes
+    If an Organisation changes:
+    - Do nothing in TC
+    - Do nothing in Hermes
+    If a Deal changes:
+    - Update the Cligency in Meta with:
+      - The new stage
+      - The new link to PD
+    - Update the Deal in Hermes with:
+      - The new stage
+      - The new status
     """
     pass
