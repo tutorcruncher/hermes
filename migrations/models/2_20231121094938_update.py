ad0a1f60535198bce67dd022593c745ce3216d03
Sebastian Prentice <seb.prentice@hotmail.com>
2023-11-21 10:02:47 +0000

    aerich migration

---
+++ migrations/models/2_20231121094938_update.py
@@ -0,0 +1,11 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        ALTER TABLE "company" ADD "utm_campaign" VARCHAR(255);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        ALTER TABLE "company" DROP COLUMN "utm_campaign";"""
