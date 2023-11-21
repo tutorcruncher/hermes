a6f046ecedf2adb004418ebe45130d4a1d731dfa
Sebastian Prentice <seb.prentice@hotmail.com>
2023-11-21 11:34:31 +0000

    fix migration

--- migrations/models/2_20231121094938_update.py
+++ migrations/models/2_20231121094938_update.py
@@ -3,9 +3,12 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "company" ADD "utm_campaign" VARCHAR(255);"""
-
+        ALTER TABLE "company" ADD "utm_campaign" VARCHAR(255);
+        ALTER TABLE "company" ADD "utm_source" VARCHAR(255);
+        """
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "company" DROP COLUMN "utm_campaign";"""
+        ALTER TABLE "company" DROP COLUMN "utm_campaign";
+        ALTER TABLE "company" DROP COLUMN "utm_source";
+        """
