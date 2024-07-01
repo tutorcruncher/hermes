2bab4cce638705c314ef0dd4ccf66d50fbcca4d6
Sebastian Prentice <seb.prentice@hotmail.com>
2024-07-01 12:00:47 +0100

    dans comments

--- app/pipedrive/views.py
+++ app/pipedrive/views.py
@@ -25,8 +25,7 @@ async def prepare_event_data(event_data: dict) -> dict:
     for hermes_id_pd_field_id in hermes_id_cf_fields:
         for state in [PDStatus.PREVIOUS.value, PDStatus.CURRENT.value]:
             if (
-                state in event_data
-                and event_data[state]
+                event_data.get('state')
                 and hermes_id_pd_field_id in event_data[state]
                 and isinstance(event_data[state][hermes_id_pd_field_id], str)
             ):
