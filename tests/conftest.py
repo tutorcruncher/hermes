502d95eef18dfc9c4de3f9a2bbbe3180f5076dfe
Sandeep <sunthapa1@hotmail.com>
2025-09-29 16:29:25 +0100

    removed local password from confest

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ def anyio_backend():
 @pytest.fixture(scope='module', autouse=True)
 def initialize_tests(request):
     # Autouse means this is always called. Used to initialise tortoise.
-    settings.pg_dsn = os.getenv('DATABASE_URL', 'postgres://postgres:waffle@localhost:5432/hermes_test')
+    settings.pg_dsn = os.getenv('DATABASE_URL', 'postgres://postgres@localhost:5432/hermes_test')
     settings.testing = True
     logfire.configure(send_to_logfire=False, token=None)
 
