322810bc5f8b689447025f58aad2e60bca2c2967
tejas <tejaschendekar@yahoo.com>
2025-11-04 15:34:06 +0000

    Added formatting

--- app/core/config.py
+++ app/core/config.py
@@ -56,7 +56,7 @@ class Settings(BaseSettings):
     pd_base_url: str = 'https://tutorcruncher.pipedrive.com'
     sync_create_deals: bool = True
     pd_api_max_rate: int = 9
-    pd_api_rate_period: int = 2 # seconds
+    pd_api_rate_period: int = 2  # seconds
 
     # Google
     g_project_id: str = 'tc-hubspot-314214'
