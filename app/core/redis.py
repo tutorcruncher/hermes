a7ae2f577802cff7a4c8a3d291b122df82566c1d
tejas <tejaschendekar@yahoo.com>
2026-04-09 11:57:05 +0100

    added changes

--- app/core/redis.py
+++ app/core/redis.py
@@ -2,4 +2,4 @@ import redis.asyncio as redis
 
 from app.core.config import settings
 
-redis_client = redis.from_url(settings.redis_url if not settings.testing else 'redis://localhost:6379')
+redis_client = redis.from_url(settings.redis_url)
