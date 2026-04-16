bcd9f440829ac15c6e172c06ad92736c7a6df5b7
tejas <tejaschendekar@yahoo.com>
2026-04-16 12:28:07 +0100

    forgot to push a commit

--- app/core/config.py
+++ app/core/config.py
@@ -56,7 +56,7 @@ class Settings(BaseSettings):
 
     # Pipedrive
     pd_api_key: str = 'test-key'
-    pd_base_url: str = 'https://tutorcruncher-test.pipedrive.com'
+    pd_base_url: str = 'https://tutorcruncher.pipedrive.com'
     sync_create_deals: bool = True
     pd_api_max_rate: int = 9
     pd_api_rate_period: int = 2  # seconds
